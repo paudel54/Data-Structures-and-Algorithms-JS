490755fdec8b84d1d103a0c40f9f6873aa98c7a2
paudel54 <2sansrit@gmail.com>
2023-01-14 18:17:05 +0545

    Worked on with time complexity, with arrays

--- BigO/5_array.js
+++ BigO/5_array.js
@@ -1,16 +1,23 @@
 let array = [2, 4, 6, 8];
 
 //pushes at end
+//O(1), single operation
 array.push(10);
 console.log(array);
 //take last element of array out
+//O(1)
 array.pop();
 console.log(array);
 
 //removes 1st array element, this causes re-indexing of array element
+//O(n)
 array.shift();
 console.log(array);
 
 //adds to first element, this causes re-indexing of array element
+//O(n)
 array.unshift(2);
 console.log(array);
+
+//note
+//searching item by index results to O(1) whereas searching by value is O(n)
