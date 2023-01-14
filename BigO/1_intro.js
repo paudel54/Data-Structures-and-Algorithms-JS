90a779932bc36b7a865305df3fa104f252f236e7
paudel54 <2sansrit@gmail.com>
2023-01-14 14:22:58 +0545

    intro to why do we need big O

---
+++ BigO/1_intro.js
@@ -0,0 +1,7 @@
+//BIG O is used to compute time complexity and space complexity in a mathematical way
+
+//Time complexity implies, total time taken to complete number of operation by machine
+//Space complexity implies, total memory space consumed by program.
+
+//consider an example, let progam A takes 10 sec to execute program B takes 8 sec to run to calculate same result but A and B can take diff approaches
+//According to time complexity Program A is more efficient 
