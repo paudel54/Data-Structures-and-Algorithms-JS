1840fc080534c28284e50e4957a3338bf9162f2e
paudel54 <2sansrit@gmail.com>
2023-01-14 15:52:33 +0545

    Clearified o(n) concepts with example

---
+++ BigO/2_o.js
@@ -0,0 +1,12 @@
+//O(n)  , its is proportional 
+//as value of n is greater number of operation is greater
+//in below code if n = 9 , it must execute 9 operations
+
+function logItems(n) {
+    for (let i = 0; i < n; i++) {
+        console.log(i);
+    }
+}
+
+logItems(9);
+//output 0,1,2,3,4,5,6,7,8. in total 9 operations
