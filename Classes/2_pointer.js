10d021a6dc89d5d3e36eb9789328229c14ab7e47
paudel54 <2sansrit@gmail.com>
2023-01-14 18:49:44 +0545

    understood why the need of pointer came into existance

---
+++ Classes/2_pointer.js
@@ -0,0 +1,14 @@
+//pointer concepts
+
+let num1 = 5;
+let num2 = num1;
+
+console.log(num1)
+console.log(num2)
+
+console.log('changing num1 value');
+num1 = 15;
+console.log(num1)
+console.log(num2)
+
+//here although num1 is changed num2 is not equal to num1 , so to fix this problem we needed the pointer
