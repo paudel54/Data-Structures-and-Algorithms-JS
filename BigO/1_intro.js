6f4508571234f84ed8f0345f62fd7e6cce38a21f
paudel54 <2sansrit@gmail.com>
2023-01-14 15:11:18 +0545

    introduction to best case finding, avg case , and worst case

--- BigO/1_intro.js
+++ BigO/1_intro.js
@@ -4,4 +4,12 @@
 //Space complexity implies, total memory space consumed by program.
 
 //consider an example, let progam A takes 10 sec to execute program B takes 8 sec to run to calculate same result but A and B can take diff approaches
-//According to time complexity Program A is more efficient 
+//According to time complexity Program A is more efficient
+
+//BIG O worst case
+
+//lets say an array 1,2,3,4,5,6,7,8
+
+//best case to search is finding 1 . best case denoted by omega headphone symbol
+//avg case to find 4, denoted by theta
+//worst case by big O i.e finding 8
