41a7a27a9ee2833793eb126202bd404dd2cc7670
paudel54 <2sansrit@gmail.com>
2023-01-26 08:20:08 +0545

    created working constructor for DLL

--- 2_double_link_list/2link.js
+++ 2_double_link_list/2link.js
@@ -1,2 +1,24 @@
 console.log('Welcome to Doubly linked List');
+// Node that creates empty single doubly link list
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
 
+}
+
+
+class DoublyLinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+        console.log('sent to node class and returned', newNode);
+        this.head = newNode;
+        this.tail = newNode;
+        this.length = 1
+    }
+}
+
+let myDoublyLinkedList = new DoublyLinkedList(7);
+console.log(myDoublyLinkedList);
