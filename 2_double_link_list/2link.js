367b50bd360f31f2013bbd25ec2682e382ada63d
paudel54 <2sansrit@gmail.com>
2023-02-11 06:09:38 +0545

    code refactored doubly linked list

--- 2_double_link_list/2link.js
+++ 2_double_link_list/2link.js
@@ -12,13 +12,42 @@ class Node {
 
 class DoublyLinkedList {
     constructor(value) {
+        //here newNode is current obj for value 1 that gets passed here  
         const newNode = new Node(value);
-        console.log('sent to node class and returned', newNode);
+        // console.log('sent to node class and returned', newNode);
         this.head = newNode;
         this.tail = newNode;
         this.length = 1
+        // console.log('newNode', newNode);
+    }
+    // creating  push method inside class 
+    push(value) {
+        const newNode = new Node(value)
+        console.log('pushed new node', newNode)
+        // if the nodes are empty list
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            // console.log('debuuggin')
+            // console.log(this.tail);
+            //this.tail points out to the current list last node here , 1
+            // console.log(this.tail.next);
+            this.tail.next = this.tail;
+            // console.log(this.tail.next);
+            // console.log('this.tail.node store node 1', this.tail.next)
+            // only one line diff between singly linked list and double is this arrow connecting. 
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++
+        return this
     }
 }
 
-let myDoublyLinkedList = new DoublyLinkedList(7);
+
+let myDoublyLinkedList = new DoublyLinkedList(1);
+console.log('inital link list before pushing', myDoublyLinkedList);
+myDoublyLinkedList.push(2);
 console.log(myDoublyLinkedList);
