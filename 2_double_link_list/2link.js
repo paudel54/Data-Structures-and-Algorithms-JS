a3765cc5858b9e4315e4fccdedb5b8ae88c89035
paudel54 <2sansrit@gmail.com>
2023-02-11 08:19:43 +0545

    insert method complete

--- 2_double_link_list/2link.js
+++ 2_double_link_list/2link.js
@@ -123,6 +123,23 @@ class DoublyLinkedList {
         }
         return false;
     }
+
+    insert(index, value) {
+        if (index === 0) return this.unshift(value);
+        if (index === this.length) return this.push(value);
+        if (index < 0 || index > this.length) return false;
+
+        const newNode = new Node(value);
+        const before = this.get(index - 1);
+        const after = before.next;
+        before.next = newNode;
+        newNode.prev = before;
+        newNode.next = after;
+        after.prev = newNode;
+        this.length++
+        return true
+    }
+
 }
 
 
