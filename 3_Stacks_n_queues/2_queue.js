5335f3af53d090d97f0f9ace7c4611d2913364ad
paudel54 <2sansrit@gmail.com>
2023-02-12 21:06:20 +0545

    implemented enqueue method for a node

--- 3_Stacks_n_queues/2_queue.js
+++ 3_Stacks_n_queues/2_queue.js
@@ -13,7 +13,23 @@ class Queue {
         this.last = newNode;
         this.length = 1
     }
+
+    enqueue(value) {
+        const newNode = new Node(value);
+        // inCase of empty Node
+        if (this.length === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
 
+
 let myQueue = new Queue(4);
+myQueue.enqueue(9);
 console.log(myQueue);
