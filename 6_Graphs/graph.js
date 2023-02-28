4ae3c465adc98e0c715ef0a1a26fbc78277ab0f6
paudel54 <2sansrit@gmail.com>
2023-02-28 08:52:08 +0545

    Graph Remove edge func accomplished

--- 6_Graphs/graph.js
+++ 6_Graphs/graph.js
@@ -31,11 +31,25 @@ class Graph {
         }
         return false
     }
+    removeEdge(vertex1, vertex2) {
+        if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+            this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2);
+            this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1);
+            return true
+        }
+        return false
+    }
 }
 
 let myGraph = new Graph();
+myGraph.addVertex('A');
 myGraph.addVertex('B');
-myGraph.addVertex('S');
+myGraph.addVertex('C');
+
 // connecting vertix B and S
-myGraph.addEdge("B", "S");
+myGraph.addEdge("A", "B");
+myGraph.addEdge("B", "C")
+myGraph.addEdge("C", "A")
+
+// myGraph.removeEdge("A", "B")
 console.log(myGraph)
