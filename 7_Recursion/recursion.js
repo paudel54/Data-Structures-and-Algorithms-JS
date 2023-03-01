b11b9977b15dc354a2b2e4f6d7cf978c4e25801a
paudel54 <2sansrit@gmail.com>
2023-03-01 07:48:42 +0545

    recursion factorial

--- 7_Recursion/recursion.js
+++ 7_Recursion/recursion.js
@@ -1,23 +1,31 @@
 // infinite loop leads to stack overflow
 // CallStack
 
-function funcThree() {
-    console.log(' i am func three')
+// function funcThree() {
+//     console.log(' i am func three')
+// }
+
+
+// function funcTwo() {
+//     funcThree()
+//     console.log('I am func Two');
+// }
+
+
+// function funcOne() {
+//     funcTwo()
+//     console.log('I am function one')
+// }
+
+// const a = funcOne();
+// console.log(a);
+
+function factorial(n) {
+    if (n === 1) return 1;
+    return n * factorial(n - 1);
+
 }
-
-
-function funcTwo() {
-    funcThree()
-    console.log('I am func Two');
-}
-
-
-function funcOne() {
-    funcTwo()
-    console.log('I am function one')
-}
-
-const a = funcOne();
+const a = factorial(4);
 console.log(a);
 
 
